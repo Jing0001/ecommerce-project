a01a566d81582c0216dc2c328282274154444773
Flora0001 <fengjing2018@outlook.com>
2024-06-02 14:09:45 -0700

    test1

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { selectIsCartOpen } from '../../store/cart/cart.selector';
+
 import { selectCurrentUser } from '../../store/user/user.selector';
 import { signOutStart } from '../../store/user/user.action';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
