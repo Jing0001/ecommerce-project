3281bf9beb9214038a4f3e4db17f2d39b60fdad0
Flora0001 <fengjing2018@outlook.com>
2024-06-02 14:06:13 -0700

    test

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -1,16 +1,12 @@
 import { Fragment } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
-
 import { selectIsCartOpen } from '../../store/cart/cart.selector';
 import { selectCurrentUser } from '../../store/user/user.selector';
 import { signOutStart } from '../../store/user/user.action';
-
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-
 import {
   NavigationContainer,
   NavLinks,
